46fb5a3ae0aa3bd2f5e49bae08e8f1f8be5a23f9
KevinJGarzonP <gar21081@byui.edu>
2022-01-14 10:02:41 -0500

    JavaScript fixed and finished

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 let currentYear = new Date().getFullYear();
 
-document.getElementById("currentYear").textContent = "New Year";
+document.getElementById("currentYear").textContent = currentYear;
 
-let lastestUpdate = new Date().getDate();
+let lastestUpdate = document.lastModified;
+
+let months = new Array("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12")
+
+let completeDate = new Date(Date.parse(lastestUpdate));
+let day = completeDate.getDate();
+let month = months[completeDate.getMonth()];
+let year = completeDate.getFullYear();
+
+
+document.getElementById("latestUpdate").textContent = month + " / " + day + " / " + year;
