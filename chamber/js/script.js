62741180ed65c540091b53f234e6bf7b3f78b29f
KevinJGarzonP <gar21081@byui.edu>
2022-02-02 15:47:52 -0500

    Week 4 home page and BoM activity start

---
+++ chamber/js/script.js
@@ -0,0 +1,15 @@
+let currentYear = new Date().getFullYear();
+
+document.getElementById("currentYear").textContent = currentYear;
+
+let lastestUpdate = document.lastModified;
+
+let months = new Array("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12")
+
+let completeDate = new Date(Date.parse(lastestUpdate));
+let day = completeDate.getDate();
+let month = months[completeDate.getMonth()];
+let year = completeDate.getFullYear();
+
+
+document.getElementById("latestUpdate").textContent = month + " / " + day + " / " + year;
