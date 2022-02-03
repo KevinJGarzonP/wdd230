25c0726eca2d72d7459cffafa49d74e01673aa23
KevinJGarzonP <gar21081@byui.edu>
2022-02-02 22:40:11 -0500

    Ham menu

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -13,3 +13,22 @@ let year = completeDate.getFullYear();
 
 
 document.getElementById("latestUpdate").textContent = month + " / " + day + " / " + year;
+
+let letter_months = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+let letter_days = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+
+let currentDate = new Date();
+
+let currentWeekDay = letter_days[currentDate.getDay()];
+let currentDay = currentDate.getDate();
+let currentMonth = letter_months[currentDate.getMonth()];
+
+document.getElementById("Current_Date").textContent = currentWeekDay + ", " + currentDay + " " + currentMonth + " " + currentYear;
+
+const menu_button = document.querySelector(".menub");
+const mainnav = document.querySelector(".navigation");
+const naveg = document.querySelector(".nav-space");
+
+menu_button.addEventListener("click", () => {mainnav.classList.toggle("responsive")}, false);
+menu_button.addEventListener("click", () => {naveg.classList.toggle("wider")}, false);
+
