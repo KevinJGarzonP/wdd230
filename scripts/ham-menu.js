6ee11d64e8d716aed1882b3916ff7534fb153b8a
KevinJGarzonP <gar21081@byui.edu>
2023-01-13 01:49:06 -0500

    Ham menu and Dark mode

---
+++ scripts/ham-menu.js
@@ -0,0 +1,18 @@
+const hamButton = document.querySelector(".menu-button");
+const items = document.querySelectorAll(".menu-item");
+
+hamButton.addEventListener("click", () => {
+    hamButton.classList.toggle("close");
+    if (hamButton.className == "menu-button close") {
+        hamButton.firstChild.src = "./images/x-button.svg";
+    } else {
+        hamButton.firstChild.src = "./images/ham-button.svg";
+    }
+    items.forEach((item) => {
+        item.classList.toggle("open");
+        
+    })
+})
+
+
+
