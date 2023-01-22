8ad06e5478a86ed05905022f0885178991c6b863
KevinJGarzonP <gar21081@byui.edu>
2023-01-22 01:28:00 -0500

    Discovery Page

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -31,3 +31,25 @@ const naveg = document.querySelector(".nav-space");
 menu_button.addEventListener("click", () => {mainnav.classList.toggle("responsive")}, false);
 menu_button.addEventListener("click", () => {naveg.classList.toggle("wider")}, false);
 
+let currentVisit = Math.floor(Date.now() / 86400000);
+const message_span = document.querySelector("#days-visit");
+
+if (!localStorage.getItem("visitTime")) {
+    let message = "This is your first visit. Welcome!";
+    message_span.textContent = message;
+    setVisitTime();
+} else {
+    lastVisit = getVisitTime();
+    numberOfDays = currentVisit - lastVisit;
+    let message = `Yout last visit was ${numberOfDays} days ago. Thanks for coming!`;
+    message_span.textContent = message;
+    setVisitTime();
+}
+
+function setVisitTime() {
+    localStorage.setItem("visitTime", JSON.stringify(currentVisit));
+}
+
+function getVisitTime() {
+    return JSON.parse(localStorage.getItem("visitTime"));
+}
